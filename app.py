2ed74d82f900c90b88b506129b297ec5131ec1ea
luizaugustoliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2026-01-26 15:37:32 -0300

    load and add the persist directory to a vector store

--- app.py
+++ app.py
@@ -33,3 +33,23 @@ def process_pdf(file):
     )
     chunks = text_spliter.split_documents(documents=docs)
     return chunks
+
+def load_existing_vector_store():
+    if os.path.exists(os.path.join(persist_directory)):
+        vector_store = Chroma(
+            persist_directory=persist_directory,
+            embedding_function=OpenAIEmbeddings(),
+        )
+        return vector_store
+    return None
+
+def add_to_vector_store(chunks, vector_store=None):
+    if vector_store:
+        vector_store.add_documents(chunks)
+    else:
+        vector_store = Chroma.from_documents(
+            documents=chunks,
+            embedding=OpenAIEmbeddings(),
+            persist_directory=persist_directory,
+        )
+    return vector_store
