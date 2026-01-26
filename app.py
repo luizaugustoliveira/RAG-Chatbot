f18adf3619c065a42788c058953e482d9ec2ada4
luizaugustoliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2026-01-26 16:07:09 -0300

    update langchain

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import streamlit as st
 
 from decouple import config
 
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_chroma import Chroma
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
 
 
 os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
