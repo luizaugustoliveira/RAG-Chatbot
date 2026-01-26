569eff4bf46226aa2a1df5720239bd7f000b5eda
luizaugustoliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2026-01-26 15:33:47 -0300

    create langchain imports and persist directory

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+import tempfile
+
+import streamlit as st
+
+from decouple import config
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+
+os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
+persist_directory = 'db'
