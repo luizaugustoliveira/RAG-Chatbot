6c1d723044b2311fa4076376a246e7b0d16c7fa4
luizaugustoliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2026-01-26 15:35:55 -0300

    define pdf loader

--- app.py
+++ app.py
@@ -16,3 +16,20 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
 persist_directory = 'db'
+
+def process_pdf(file):
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
+        temp_file.write(file.read())
+        temp_file_path = temp_file.name
+
+    loader = PyPDFLoader(temp_file_path)
+    docs = loader.load()
+
+    os.remove(temp_file_path)
+
+    text_spliter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=400,
+    )
+    chunks = text_spliter.split_documents(documents=docs)
+    return chunks
