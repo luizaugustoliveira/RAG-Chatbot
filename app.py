e0fb166ad44d088d93eda73b99145d9a3c78f933
luizaugustoliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2026-01-26 15:38:00 -0300

    create a retriever

--- app.py
+++ app.py
@@ -53,3 +53,33 @@ def add_to_vector_store(chunks, vector_store=None):
             persist_directory=persist_directory,
         )
     return vector_store
+
+def ask_question(model, query, vector_store):
+    llm = ChatOpenAI(model=model)
+    retriever = vector_store.as_retriever()
+
+    system_prompt = '''
+    Use o contexto para responder as perguntas.
+    Se não encontrar uma resposta no contexto,
+    explique que não há informações disponíveis.
+    Responda em formato de markdown e com visualizações
+    elaboradas e interativas.
+    Contexto: {context}
+    '''
+    messages = [('system', system_prompt)]
+    for message in st.session_state.messages:
+        messages.append((message.get('role'), message.get('content')))
+    messages.append(('human', '{input}'))
+
+    prompt = ChatPromptTemplate.from_messages(messages)
+
+    question_answer_chain = create_stuff_documents_chain(
+        llm=llm,
+        prompt=prompt,
+    )
+    chain = create_retrieval_chain(
+        retriever=retriever,
+        combine_docs_chain=question_answer_chain,
+    )
+    response = chain.invoke({'input': query})
+    return response.get('answer')
